8adb106b70760df5c7ddd93520e1e1b35d680773
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-09-12 20:09:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Forge
+
+[Remodel](https://github.com/rojo-rbx/remodel) script for automatically merging place files and Rojo projects together
+then publishing to Roblox. Example repository can be found [here](https://github.com/raphtalia/RemodelTest).
---
+++ main.lua
@@ -0,0 +1,123 @@
+local success, config = pcall(function()
+    return json.fromString(remodel.readFile("deployment.json"))
+end)
+if not success then
+    error("Could not read deployment.json: " .. config)
+end
+
+local branchName = io.popen("git rev-parse --abbrev-ref HEAD"):read("*l")
+local commitSHA = io.popen("git rev-parse HEAD"):read("*l")
+local assetId
+local dataModels = {}
+
+local targetType = config.targetType
+if targetType == "string" then
+    assetId = config.target
+elseif targetType == "table" then
+    assetId = config.target[branchName]
+
+    if not assetId then
+        -- This branch has no specified target, skip rest of script
+        return
+    end
+else
+    error("Invalid targetType: " .. targetType)
+end
+
+if #config.files > 0 then
+    -- Read the place files into DataModels
+    for i, fileParams in ipairs(config.files) do
+        local path = fileParams.path
+        local fileExt = path:sub(-5)
+
+        if remodel.isFile(path) and fileExt == ".rbxl" or fileExt == ".rbxm" then
+            table.insert(dataModels, remodel.readPlaceFile(path))
+        elseif remodel.isDir(path) then
+            local outputPath = i.. ".rbxl"
+            os.execute(("rojo build --output %s %s"):format(outputPath, path))
+            table.insert(dataModels, remodel.readPlaceFile(outputPath))
+        end
+
+        --[[
+        local script = fileParams.script
+        if script then
+
+        end
+        ]]
+    end
+else
+    error("Expected 1 or more file paths")
+end
+
+local function findFirstChildOfClassAndName(parent, className, name)
+    for _, child in ipairs(parent:GetChildren()) do
+        if child.ClassName == className and child.Name == name then
+            return child
+        end
+    end
+end
+
+local function mergeChildren(parent1, parent2)
+    for _,child2 in ipairs(parent2:GetChildren()) do
+        local child1 = findFirstChildOfClassAndName(parent1, child2.ClassName, child2.Name)
+        if child1 then
+            mergeChildren(child1, child2)
+        else
+            child2.Parent = parent1
+        end
+    end
+end
+
+local function mergeDataModels(dataModel1, dataModel2)
+    -- Transfer services if DataModel1 is missing services that DataModel2 has
+    for _,service in ipairs(dataModel2:GetChildren()) do
+        if not dataModel1:FindFirstChildOfClass(service.ClassName) then
+            service.Parent = dataModel1
+        end
+    end
+
+    for _,service1 in ipairs(dataModel1:GetChildren()) do
+        local service2 = dataModel2:FindFirstChildOfClass(service1.ClassName)
+
+        if service2 then
+            for _,child2 in ipairs(service2:GetChildren()) do
+                local child1 = findFirstChildOfClassAndName(service1, child2.ClassName, child2.Name)
+                if child1 then
+                    print("Merging", child2:GetFullName())
+                    mergeChildren(child1, child2)
+                else
+                    print("Copying", child2:GetFullName())
+                    child2.Parent = service1
+                end
+            end
+        end
+    end
+end
+
+-- Merge all the DataModels in order of arguments
+while #dataModels > 1 do
+    mergeDataModels(dataModels[#dataModels - 1], table.remove(dataModels, #dataModels))
+end
+
+local dataModel = dataModels[1]
+
+if config.includeMetadata then
+    -- Add commit metadata to the DataModel
+    local metadata = Instance.new("ModuleScript")
+    metadata.Name = "_GithubMetadata"
+    remodel.setRawProperty(
+        metadata,
+        "Source",
+        "String",
+        [[
+            return {
+                Branch = "]] .. branchName .. [[",
+                Commit = "]] .. commitSHA .. [[",
+            }
+        ]]
+    )
+    metadata.Parent = dataModel:GetService("ReplicatedStorage")
+end
+
+-- Publish the DataModel to Roblox
+remodel.writeExistingPlaceAsset(dataModel, assetId)
